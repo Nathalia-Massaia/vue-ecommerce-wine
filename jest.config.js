afe7f8ba5455481d263f2c3c39e65787ed11e456
nathalia-massaia <nathaliamassaia@gmail.com>
2021-10-15 02:56:42 -0400

    finish cart behavior

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
-  testMatch: ['**/src/components/**/tests.ts'],
-  collectCoverage: true,
-  collectCoverageFrom: [
-    'src/components/**/*.vue',
-    '!src/views/**/*.vue',
-  ],
-};
